f47b3213792df2e4b4a42b7cd60aaf7119b22195
Avisekh gurung <68841535+avisekhgurung@users.noreply.github.com>
2025-02-08 09:50:48 +0530

    model

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Glamourhall | AI-Powered Fashion Advice, Just for You",
   description: "Receive personalized fashion recommendations, outfit ideas, and style tips with Glamourhall. Your AI-powered fashion assistant helps you discover the latest trends and perfect your unique look.",
+  icons: {
+    icon: '/model.ico'
+  },
 };
 
 
